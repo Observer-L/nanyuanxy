fc828493b6301ec2ebb911f7d976e7db7a6682a5
Observer-L <observer_l@163.com>
2019-03-29 21:11:09 +0800

    优化picker逻辑

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,13 @@
 import school from './school'
 import {markerStyle} from './map'
 import areas from './areas'
+import itemTypes from './itemTypes'
 
 export default {
   DEBUG: true, // true调用本地数据, false使用云开发能力
   env: __ENV_PROD__ ? 'xy-pro' : 'xy-dev',
   school,
   markerStyle,
-  areas
+  areas,
+  itemTypes
 }
