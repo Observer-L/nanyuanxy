7d542bbd0932979fd3c8acc7c79a2ad9fba9f9b7
Observer-L <observer_l@163.com>
2019-03-31 10:48:44 +0800

    report页面基本完成，TODO入库

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -1,7 +1,15 @@
-var moment = require('moment');
+import moment from 'moment'
+import 'moment/locale/zh-cn'
+
+moment.locale('zh-cn')
 
 export function formatTime2Duration(time) {
   // https://momentjs.com/docs/#/durations
   time = parseInt(time)
   return moment(moment.duration(time, 'seconds')._data).format('mm:ss')
+}
+
+export function format2FullTime(time) {
+  const format = 'YYYY-M-D a h:mm'
+  return moment(time).format(format)
 }
