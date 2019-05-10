b12e7958603f316cd3fc12b7c13764d68af9fad3
Observer-L <observer_l@163.com>
2019-05-10 16:05:44 +0800

    add account pages & entry

---
+++ cloudfunction/saveUserInfo/index.js
@@ -0,0 +1,29 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+
+const db = cloud.database()
+const _ = db.command
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  const wxContext = cloud.getWXContext()
+
+  if (event.new) {
+    await db.collection('users').add({
+      data: {
+        openid: wxContext.APPID,
+        ...event
+      }
+    })
+  } else {
+    await db.collection('users').where(
+      {openid: _.eq(wxContext.OPENID)}
+    ).update({
+      data: event
+    })
+  }
+
+  return {}
+}
