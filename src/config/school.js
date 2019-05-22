7d8a29c3b47ec8a8c37455c4e3771b7dde3e0b90
Observer-L <observer_l@163.com>
2019-05-22 18:17:18 +0800

    switch mode to timing messages

--- src/config/school.js
+++ src/config/school.js
@@ -2,12 +2,13 @@ export default {
   id: 9999999999,
   name: '中山大学南方学院',
   short_name: '南苑',
-  location: [23.632674, 113.679404],
+  latitude: 23.632674,
+  longitude: 113.679404,
   scale: 16,
   logo: 'nfsysu',
   music: {
     // src: 'cloud://xy-dev.7879-xy-dev/music/nanfang.mp3',
-    src: 'http://dl.stream.qqmusic.qq.com/M500000HTjAv1VHVnA.mp3?vkey=1CE0A9EB8BF65413A037CB0989399D93B031FC659949B00FE3F493A8F6A05E58FE23758F0582030A49EF0AB0D8D12C5C7DB9293A23302590&guid=5150825362&fromtag=1',
+    src: 'https://7879-xy-dev-1258862796.tcb.qcloud.la/music/nanfang.mp3?sign=579767721f4ec5d7395e110cb75a2577&t=1558499246',
     poster: 'https://7879-xy-dev-1258862796.tcb.qcloud.la/music/nanfang.jpg?sign=3dda30bda4ffbe7d343b30c4d0470c41&t=1557238308',
     author: '卢佳楠 / 谢维嘉',
     name: '南芳'
@@ -21,5 +22,5 @@ export default {
     phone: '123456789',
     address: '校园地址'
   },
-  panorama: 2291778 // 720云全景场景ID
+  panorama: 2291778   // 720云全景场景ID
 }
